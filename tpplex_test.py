196474672bb5611559f11a7caecd2e51c5bcb993
Rogério Aparecido Gonçalves <rogerio.rag@gmail.com>
2023-03-16 14:26:48 -0300

    Adding test files

---
+++ tpplex_test.py
@@ -0,0 +1,32 @@
+import tpplex
+import subprocess
+
+def execute_test(input_file):
+    path_file = 'tests/' + input_file
+    process = subprocess.Popen(['python', 'tpplex.py', path_file],
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    stdout, stderr
+
+    output_file = open(path_file + ".out", "r")
+
+    #read whole file to a string
+    expected_output = output_file.read()
+
+    output_file.close()
+
+    print(stdout)
+    print(expected_output)
+
+    return stdout.decode("utf-8") == expected_output
+
+
+def teste_001():
+    assert execute_test("teste-001.tpp") == True
+
+def teste_002():
+    assert execute_test("teste-002.tpp") == True
+
+def teste_003():
+    assert execute_test("teste-003.tpp") == True
